5db13bc2a3f2d351b9e6f679a10b5351b58d4987
Venkata Chebiyyam <venkatac@qti.qualcomm.com>
2018-02-07 17:05:12 -0800

    jj

--- smile.py
+++ smile.py
@@ -12,10 +12,11 @@ run = wandb.init()
 config = run.config
 
 config.epochs=100
+config.batch_size=32
 config.first_layer_conv_width=5
 config.first_layer_conv_height=5
-config.dense_layer_size=1024
-config.dropout=0.1
+config.dense_layer_size=256
+config.dropout=0.25
 
 # load data
 train_X, train_y, test_X, test_y = smiledataset.load_data()
@@ -40,9 +41,10 @@ for ii in range(train_X.shape[0]):
     edge_map = np.float32(255*edge_map /  np.max(edge_map))
     train_X_edge[ii,:,:,0] = edge_map
 train_X_temp = np.append(train_X,train_X_edge,axis=3)
-train_X = train_X_temp
-test_X_edge = test_X 
+#train_X = train_X_temp
 
+train_X = train_X_edge
+test_X_edge = test_X
 # Augment image with gradient map
 for ii in range(test_X.shape[0]):
     edge_map = cv2.Sobel(test_X[ii,:,:,0], cv2.COLOR_BGR2GRAY, 0, 1)
@@ -50,7 +52,8 @@ for ii in range(test_X.shape[0]):
     edge_map = np.float32(255*edge_map /  np.max(edge_map))
     test_X_edge[ii,:,:,0] = edge_map
 test_X_temp = np.append(test_X,test_X_edge,axis=3)
-test_X = test_X_temp
+#test_X = test_X_temp
+test_X = test_X_edge
 #Add train data
 
 train_X /= 255.0
@@ -61,14 +64,16 @@ print "%d" %config.first_layer_conv_width
 model = Sequential()
 model.add(Conv2D(32,
     (config.first_layer_conv_width, config.first_layer_conv_height),
-    input_shape=(32, 32, 2),
-    activation='relu'))
-model.add(Conv2D(32,
-    (config.first_layer_conv_width, config.first_layer_conv_height),
-    input_shape=(32, 32, 32),
+    input_shape=(32, 32, 1),
     activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(config.dropout))
+model.add(Dropout(0.2))
+#model.add(Conv2D(64,
+#    (config.first_layer_conv_width, config.first_layer_conv_height),
+#    input_shape=(16, 16, 64),
+#    activation='relu'))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Dropout(0.2))
 model.add(Flatten())
 model.add(Dense(config.dense_layer_size, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
@@ -79,7 +84,7 @@ model.compile(loss='categorical_crossentropy', optimizer='adam',
 
 #model.add(Flatten(input_shape=(img_rows, img_cols,1)))
 #model.add(Dense(num_classes, activation='softmax') )
-#model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) 
 
 model.fit(train_X, train_y,
     epochs=config.epochs, verbose=1,
