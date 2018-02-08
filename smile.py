0d17fe65410727c2909a91641d41b91f56f17430
Venkata Chebiyyam <venkatac@qti.qualcomm.com>
2018-02-07 16:41:13 -0800

    aa

--- smile.py
+++ smile.py
@@ -33,27 +33,27 @@ img_rows, img_cols = train_X.shape[1:]
 test_X = test_X.reshape(test_X.shape[0], test_X.shape[1], test_X.shape[2], 1)
 train_X = train_X.reshape(train_X.shape[0], train_X.shape[1], train_X.shape[2], 1)
 
-# #Add train data
-# train_X_edge = train_X
-# # Augment image with gradient map
-# for ii in range(train_X.shape[0]):
-    # edge_map = cv2.Sobel(train_X[ii,:,:,0], cv2.COLOR_BGR2GRAY, 0, 1)
-    # edge_map = edge_map - np.min(edge_map)
-    # edge_map = np.float32(255*edge_map /  np.max(edge_map))
-    # train_X_edge[ii,:,:,0] = edge_map
-# train_X_temp = np.append(train_X,train_X_edge,axis=3)
-# train_X = train_X_temp
-# test_X_edge = test_X
+#Add train data
+train_X_edge = train_X
+# Augment image with gradient map
+for ii in range(train_X.shape[0]):
+    edge_map = cv2.Sobel(train_X[ii,:,:,0], cv2.COLOR_BGR2GRAY, 0, 1)
+    edge_map = edge_map - np.min(edge_map)
+    edge_map = np.float32(255*edge_map /  np.max(edge_map))
+    train_X_edge[ii,:,:,0] = edge_map
+train_X_temp = np.append(train_X,train_X_edge,axis=3)
+train_X = train_X_temp
+test_X_edge = test_X
 
-# # Augment image with gradient map
-# for ii in range(test_X.shape[0]):
-    # edge_map = cv2.Sobel(test_X[ii,:,:,0], cv2.COLOR_BGR2GRAY, 0, 1)
-    # edge_map = edge_map - np.min(edge_map)
-    # edge_map = np.float32(255*edge_map /  np.max(edge_map))
-    # test_X_edge[ii,:,:,0] = edge_map
-# test_X_temp = np.append(test_X,test_X_edge,axis=3)
-# test_X = test_X_temp
-# #Add train data
+# Augment image with gradient map
+for ii in range(test_X.shape[0]):
+    edge_map = cv2.Sobel(test_X[ii,:,:,0], cv2.COLOR_BGR2GRAY, 0, 1)
+    edge_map = edge_map - np.min(edge_map)
+    edge_map = np.float32(255*edge_map /  np.max(edge_map))
+    test_X_edge[ii,:,:,0] = edge_map
+test_X_temp = np.append(test_X,test_X_edge,axis=3)
+test_X = test_X_temp
+#Add train data
 
 train_X /= 255.0
 test_X /= 255.0
